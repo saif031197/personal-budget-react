2a6bcb20f61ff8d5676dae255e01efd9237c830f
Saif <31683388+saif031197@users.noreply.github.com>
2020-10-14 16:46:38 -0400

    Porting to react

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer class="bottom">
+        <footer class="center">
+            All rights reserved &copy; Fabio Nolasco
+        </footer>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Menu/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+    Link
+  } from "react-router-dom";
+
+
+
+function Menu() {
+  return (
+    <nav class="menu">
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><a rel="canonical" aria-label="Google Home" href="https://google.com">Google</a></li>
+            <li><a rel="canonical" aria-label="Github" href="https://github.com/saif031197/personal-budget-react">Github</a></li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Menu;
